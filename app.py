ee177fa45009e7feaeed4bcec966380059a683ff
gsaisudheer <gsaisudheer@gmail.com>
2025-07-09 07:26:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+@app.route('/product1')
+def product1():
+    return render_template("product1.html")
+
+@app.route('/product2')
+def product2():
+    return render_template("product2.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
